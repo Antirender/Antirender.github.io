2c3ff66dc29bbd7a7ff7031e371a4556ab27a7e5
Antirender <z487213030@gmail.com>
2025-09-03 10:34:47 -0400

    Add files via upload

---
+++ template/js/main.js
@@ -0,0 +1,5 @@
+let panel=document.getElementById("panel")
+panel.innerHTML = "This is message from html";
+panel.innerHTML += "<br><img src='../img/js.png'>";
+panel.insertAdjacentText('afterbegin','some text')
+panel.insertAdjacentText()'afterbegin',<p>some 123 </p>
